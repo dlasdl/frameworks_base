82b77231f8b7fcf62552155892c8d15c76f0d38f
Anushek Prasal <anushekprasal@gmail.com>
2019-02-04 19:59:50 +0530

    base: QS header improvements
    - Fix the gradient behind the qs
    - Fix the qs panel layout
    - Detach the header image from the top
    - Set the bottom margin only when custom header enabled
    
    Change-Id: Ic6af409145cceb9992795a96f37c0b66c757d6fa
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -22,6 +22,7 @@ import android.annotation.ColorInt;
 import android.app.ActivityManager;
 import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -347,9 +348,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
                 resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
 
-        int statusBarBottomMargin = resources.getDimensionPixelSize(
-                R.dimen.qs_header_image_bottom_margin);
-
         mSystemIconsView.getLayoutParams().height = topMargin;
         mSystemIconsView.setLayoutParams(mSystemIconsView.getLayoutParams());
 
@@ -363,9 +361,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             if (mHeaderImageEnabled) {
                 qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
             }
-            // always add the margin below the statusbar with or without image
-            qsHeight += statusBarBottomMargin;
-
             lp.height = Math.max(getMinimumHeight(), qsHeight);
         }
 
