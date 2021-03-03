29c41a870ca01f8821a3c98bec644615ef02d0ba
spezi77 <spezi7713@gmx.net>
2021-03-03 16:19:54 +0100

    NotificationManagerService: Fix usage of SystemClock after merge of r32.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5581,7 +5581,7 @@ public class NotificationManagerService extends SystemService {
                 mUsageStats.dump(pw, "    ", filter);
             }
 
-            long now = SystemClock.elapsedRealtime();
+            long now = mSystemClock.elapsedRealtime();
             pw.println("\n  Last notification sound timestamps:");
             for (Map.Entry<String, Long> entry : mLastSoundTimestamps.entrySet()) {
                 pw.print("    " + entry.getKey() + " -> ");
@@ -7007,7 +7007,7 @@ public class NotificationManagerService extends SystemService {
         }
         if (buzz || beep) {
             mLastSoundTimestamps.put(generateLastSoundTimeoutKey(record),
-                    SystemClock.elapsedRealtime());
+                    mSystemClock.elapsedRealtime());
         }
         final int buzzBeepBlink = (buzz ? 1 : 0) | (beep ? 2 : 0) | (blink ? 4 : 0);
         if (buzzBeepBlink > 0) {
@@ -7050,7 +7050,7 @@ public class NotificationManagerService extends SystemService {
         if (value == null) {
             return false;
         }
-        return SystemClock.elapsedRealtime() - value < timeoutMillis;
+        return mSystemClock.elapsedRealtime() - value < timeoutMillis;
     }
 
     private String generateLastSoundTimeoutKey(NotificationRecord record) {
